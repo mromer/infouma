9fbad41ec6b7c3b3820e47be86670b2ca6c10f93
marcoEdoctores <marco.romero@edoctores.com>
2013-03-18 20:07:48 +0100

    Se realizan cambios. Host...

--- infoschool-html5/cms/secure.js
+++ infoschool-html5/cms/secure.js
@@ -1,11 +1,11 @@
 var request = $.ajax({
-	type: 'GET',
-	url: 'http://localhost:8080/infoschool/schoolrest/getstatus'
+    type: 'GET',
+    url: host + '/infoschool/schoolrest/getstatus'
 
 });
 
 request.success(function(data, textStatus, jqXHR) {
-	if (data == 'no_authenticated') {
+    if (data == 'no_authenticated') {
 		window.location.replace("index.html");
 	} else {
 		doPostValidate();
@@ -13,5 +13,5 @@ request.success(function(data, textStatus, jqXHR) {
 });
 
 request.error(function(jqXHR, textStatus, errorThrown) {
-	alert('error:' + errorThrown);
+   alert('error:' + errorThrown);
 });
